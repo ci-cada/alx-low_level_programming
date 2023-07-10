52678c3a8e372ebc1bc14dd830a89408bb1e7e1c
ci-cada <ciphedc@gmail.com>
2023-07-10 17:04:29 +0300

    File I/O Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints to the STDOUT
- * @filename: name of the file to be read
- * @letters: maximum number of letters to be read and printed
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: actual number of letters it could read and print
- * if filename is NULL return 0
- * if write fails return 0
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
-	int file, length, var, out;
-	char *checker;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-	if (file == -1)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	checker = malloc(sizeof(char) * letters);
-	if (checker == NULL)
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
 
-	read(file, checker, letters);
-	checker[letters] = '\0';
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	for (var = 0; checker[var] != '\0'; var++)
-		length += 1;
+	close(fd);
 
-	out = close(file);
-	if (out != 0)
-		exit(-1);
+	free(buf);
 
-	out = write(STDOUT_FILENO, checker, length);
-	if (out != length)
-		return (0);
-
-	free(checker);
-
-	return (length);
+	return (nwr);
 }
