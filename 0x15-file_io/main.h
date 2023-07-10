b11b5bb400854ebe0bcf642f136eb4759a6ea4bd
ci-cada <ciphedc@gmail.com>
2023-07-10 16:37:48 +0300

    File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,23 +6,12 @@
 #include <sys/stat.h>
 
 
-int _putchar(char c);
+int putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 
 void error_98(int fd_to, char *buffer, char *argv);
 void error_99(int fd_to, char *buffer, char *argv);
 void error_100(int fd_t0, char *buffer);
 
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
